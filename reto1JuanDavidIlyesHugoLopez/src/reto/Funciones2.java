892dfffb3b5dba47a92809a13beaace7cc952577
vhugolm2023 <victo@Vivi>
2025-11-17 13:12:19 +0100

    Funciones 2

--- reto1JuanDavidIlyesHugoLopez/src/reto/Funciones2.java
+++ reto1JuanDavidIlyesHugoLopez/src/reto/Funciones2.java
@@ -1,5 +1,25 @@
 package reto;
 
 public class Funciones2 {
-
+	public static int sumar(int numeroa, int numerob){
+		int suma=numeroa+numerob;
+		return suma;
+	}
+	
+	public static int restar(int numeroc, int numerod){
+		int resta=numeroc-numerod;
+		return resta;
+	}
+	public static boolean esPar(int numeroe) {
+		if(numeroe%2==0) {
+			return true;
+		}
+		else return false;
+	}
+	public static boolean esImpar(int numerof) {
+		if(numerof%2!=0) {
+			return true;
+		}
+		else return false;
+	}
 }

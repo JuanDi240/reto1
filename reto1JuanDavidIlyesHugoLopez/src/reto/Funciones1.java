687372f8159d72adc2ad481b80ce32aa9ed1e79c
JuanDi240 <1DAM@A11-PC30>
2025-11-17 12:07:54 +0100

    Funciones 1 completo

--- reto1JuanDavidIlyesHugoLopez/src/reto/Funciones1.java
+++ reto1JuanDavidIlyesHugoLopez/src/reto/Funciones1.java
@@ -2,4 +2,17 @@ package reto;
 
 public class Funciones1 {
 
+public static int maximo3(int n1, int n2, int n3) {
+	int numeroMaximo = Math.max(n1, n2);
+	numeroMaximo = Math.max(numeroMaximo, n3);
+	return numeroMaximo;
+}
+	
+public static int minimo3(int n1, int n2, int n3) {
+	int numeroMinimo = Math.min(n1, n2);
+	numeroMinimo = Math.min(numeroMinimo, n3);
+	return numeroMinimo;
+}	
+	
+	
 }

0a3953af578000ceef0b50fcd9432a9ae2dd5384
ilyes <ilyes@DESKTOP-K7TNL3J>
2025-11-17 13:08:55 +0100

    Funciones 3 mejorado

--- reto1JuanDavidIlyesHugoLopez/src/reto/Funciones3.java
+++ reto1JuanDavidIlyesHugoLopez/src/reto/Funciones3.java
@@ -1,5 +1,29 @@
 package reto;
 
 public class Funciones3 {
-
+	public static int multiplicar(int a, int b) {
+		int resultado = a*b;
+		return resultado;
+	}
+	public static int division(int a, int b) {
+		if (a == 0 || b == 0) {
+			return 0;
+		}else {
+			return a/b;
+		}
+	}
+	public static boolean esPositivo(int n) {
+		if (n >= 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static boolean esNegativo(int n) {
+		if (n <= 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
